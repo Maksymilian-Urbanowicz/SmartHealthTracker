64e4f78b8e0ff254aa6f3b989c2ecf722efafa04
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2023-09-15 08:31:59 +0200

    begin implement Google Maps activity

---
+++ app/src/main/java/com/example/application_gps_project/GoogleMaps.java
@@ -0,0 +1,14 @@
+package com.example.application_gps_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoogleMaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps);
+    }
+}
