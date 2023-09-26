af24f1813e074b0e9f7a004335f32a9fdd65f37f
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2023-09-26 11:35:46 +0200

    Implement road activity. Draw road between 2 different cities

--- app/src/main/java/com/example/application_gps_project/GoogleMaps_Activity.java
+++ app/src/main/java/com/example/application_gps_project/GoogleMaps_Activity.java
@@ -1,34 +1,50 @@
 package com.example.application_gps_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.application_gps_project.databinding.ActivityLocationBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import java.io.IOException;
+import java.util.List;
 
 public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
-
+    private LatLng fromLatLng = null;
+    private LatLng toLatLng = null;
     private GoogleMap mMap;
     private LocationManager locationManager;
 
@@ -47,24 +63,107 @@ public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReady
         binding = ActivityLocationBinding.inflate(getLayoutInflater());
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(GoogleMaps_Activity.this);
+        mapFragment.getMapAsync(this);
 
-        FloatingActionButton FAB_FolowLocalisation = findViewById(R.id.FAB_FolowLocalisation);
-        FAB_FolowLocalisation.setOnClickListener(new View.OnClickListener() {
+        Button btnFindRoute = findViewById(R.id.btnFind_Route);
+        btnFindRoute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //set ON / OFF following current location
-                isFollowLocation=!isFollowLocation;
-                Toast.makeText(GoogleMaps_Activity.this, "Tracing localisation: " + ((isFollowLocation == true)? "ON": "OFF"), Toast.LENGTH_SHORT).show();
-
-                //set icon
-                int nowyObrazSrc = (isFollowLocation==true)?R.drawable.baseline_near_me :R.drawable.baseline_near_me_disabled;
-                Drawable drawable = getResources().getDrawable(nowyObrazSrc);
-                FAB_FolowLocalisation.setImageDrawable(drawable);
+                LinearLayout LLfindRoute = findViewById(R.id.LLfind_Route);
+                LLfindRoute.setVisibility(View.VISIBLE);
             }
         });
 
+        Button btnSetRoute = findViewById(R.id.btnSet_Route);
+        btnSetRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Geocoder geocoder = new Geocoder(GoogleMaps_Activity.this);
+                List<Address> addresses;
 
+                try {
+                    EditText TEdestFrom = findViewById(R.id.TEdest_From);
+                    EditText TEdestTo = findViewById(R.id.TEdest_TO);
+                    addresses = geocoder.getFromLocationName(String.valueOf(TEdestFrom.getText()), 1);
+                    if (!addresses.isEmpty()) {
+                        Address address = addresses.get(0);
+                        fromLatLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    }
+
+                    addresses = geocoder.getFromLocationName(String.valueOf(TEdestTo.getText()), 1);
+                    if (!addresses.isEmpty()) {
+                        Address address = addresses.get(0);
+                        toLatLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    }
+
+                    drawRouteOnMap();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        FloatingActionButton FABFollowLocation = findViewById(R.id.FAB_FolowLocalisation);
+        FABFollowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //set ON / OFF following current location
+                isFollowLocation = !isFollowLocation;
+                Toast.makeText(GoogleMaps_Activity.this, "Tracing localisation: " + ((isFollowLocation == true) ? "ON" : "OFF"), Toast.LENGTH_SHORT).show();
+
+                //set icon
+                int nowyObrazSrc = (isFollowLocation == true) ? R.drawable.baseline_near_me : R.drawable.baseline_near_me_disabled;
+                Drawable drawable = getResources().getDrawable(nowyObrazSrc);
+                FABFollowLocation.setImageDrawable(drawable);
+            }
+        });
+    }
+
+    private void drawRouteOnMap() {
+        if (mMap != null && fromLatLng != null && toLatLng != null) {
+            String googleMapsApiKey = BuildConfig.API_KEY;
+            GeoApiContext geoApiContext = new GeoApiContext.Builder()
+                    .apiKey(googleMapsApiKey)
+                    .build();
+
+            try {
+                DirectionsResult result = DirectionsApi.newRequest(geoApiContext)
+                        .mode(TravelMode.DRIVING) // Możesz zmienić tryb na inny, np. TravelMode.WALKING
+                        .origin(new com.google.maps.model.LatLng(fromLatLng.latitude, fromLatLng.longitude))
+                        .destination(new com.google.maps.model.LatLng(toLatLng.latitude, toLatLng.longitude))
+                        .await();
+
+                if (result.routes != null && result.routes.length > 0) {
+                    // Uzyskaj szczegółowe punkty trasy
+                    List<com.google.maps.model.LatLng> path = result.routes[0].overviewPolyline.decodePath();
+
+                    // Utwórz obiekt do rysowania trasy
+                    PolylineOptions polylineOptions = new PolylineOptions()
+                            .width(15) // Grubość trasy
+                            .color(Color.RED); // Kolor trasy
+
+                    // Dodaj punkty trasy do obiektu PolylineOptions
+                    for (com.google.maps.model.LatLng point : path) {
+                        polylineOptions.add(new LatLng(point.lat, point.lng));
+                    }
+
+                    // Dodaj trasę do mapy
+                    mMap.clear();
+                    mMap.addPolyline(polylineOptions);
+
+                    // Przesuń kamerę na środek trasy
+                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                    builder.include(fromLatLng);
+                    builder.include(toLatLng);
+                    LatLngBounds bounds = builder.build();
+                    int padding = 100; // Odstęp od krawędzi mapy
+                    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                    mMap.moveCamera(cu);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -88,15 +187,13 @@ public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReady
                     // Ustaw odpowiedni zoom
                     float zoomLevel = 19.0f; // Możesz dostosować wartość zoomu
                     LatLng markerPosition = new LatLng(latitude, longitude);
-                    //mMap.addMarker(new MarkerOptions().position(markerPosition).title("Current location marker"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, zoomLevel));
 
-                    FloatingActionButton FAB_ShareLocation = findViewById(R.id.FAB_ShareLocation);
-                    FAB_ShareLocation.setOnClickListener(new View.OnClickListener() {
+                    FloatingActionButton FABShareLocation = findViewById(R.id.FAB_ShareLocation);
+                    FABShareLocation.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                             // Tworzymy link URL na podstawie współrzędnych
-
                             String locationUrl = "https://maps.google.com/?q=" + String.valueOf(latitude) + "," + String.valueOf(longitude);
 
                             // Tworzymy Intencję do udostępnienia linku URL
@@ -108,7 +205,6 @@ public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReady
                             startActivity(Intent.createChooser(intent, "Udostępnij lokalizację"));
                         }
                     });
-
                 }
             }).addOnFailureListener(this, e -> {
                 // Obsłuż błąd, jeśli nie udało się pobrać lokalizacji.
@@ -125,9 +221,8 @@ public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReady
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        //Toast.makeText(this, "" + isFollowLocation, Toast.LENGTH_LONG).show();
         // Aktualizacja lokalizacji użytkownika
-        if (isFollowLocation == true) {
+        if (isFollowLocation) {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
 
@@ -135,11 +230,10 @@ public class GoogleMaps_Activity extends AppCompatActivity implements OnMapReady
             float zoomLevel = 19.0f; // Możesz dostosować wartość zoomu
             LatLng markerPosition = new LatLng(latitude, longitude);
             mMap.clear(); // Wyczyść wszystkie markery na mapie
-            //mMap.addMarker(new MarkerOptions().position(markerPosition).title("Current location marker"));
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, zoomLevel));
-            //Toast.makeText(GoogleMaps_Activity.this, "SiemA DZIAŁAM W GOOGLEMAPS_Activity", Toast.LENGTH_LONG).show();
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
